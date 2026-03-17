2794cf3946d24df62112bd1c90ed58000f367013
Serhiy <seregarachuk20@gmail.com>
2026-03-17 19:09:39 +0200

    Styled components third-party package

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -1,4 +1,13 @@
 import { useState } from 'react';
+import { styled } from 'styled-components';
+
+const ControlContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  margin-bottom: 1.5rem;
+`
+
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
@@ -21,8 +30,8 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <div className="controls">
+    <div id="auth-inputs"> 
+    <ControlContainer>       
         <p  className='paragraph'>
           <label className={`label ${emailNotValid ?  "invalid" : ''  }`}>Email</label>
           <input
@@ -40,8 +49,9 @@ export default function AuthInputs() {
               handleInputChange('password', event.target.value)
             }
           />
-        </p>
-      </div>  
+        </p>         
+      
+      </ControlContainer>  
       <div className="actions">
         <button type="button" className="text-button">
           Create a new account
